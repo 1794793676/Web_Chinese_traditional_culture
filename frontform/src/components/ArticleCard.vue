1c95b231f5c1d09c3f091070ed454eff08f4eb61
Dolphin_CN <1794793676@qq.com>
2026-05-21 16:44:32 +0800

    feat(front): map local cover images and update home hero background

--- frontform/src/components/ArticleCard.vue
+++ frontform/src/components/ArticleCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="card article-card" @click="openArticle">
-    <div v-if="article.coverUrl" class="article-card__image">
-      <img :src="article.coverUrl" :alt="article.title" :style="{ display: coverFailed ? 'none' : 'block' }" @error="coverFailed = true" />
+    <div class="article-card__image">
+      <img :src="displayCoverUrl" :alt="article.title" :style="{ display: coverFailed ? 'none' : 'block' }" @error="coverFailed = true" />
       <div v-if="coverFailed" class="image-fallback">华夏文脉</div>
     </div>
     <div class="article-card__body">
@@ -19,6 +19,13 @@
 <script setup>
 import { computed, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
+import coverDefault from '../picture/华夏文脉数字展馆背景图.png'
+import coverCalligraphy from '../picture/华夏文脉数字展馆背景图-书法之美.png'
+import coverSolarTerms from '../picture/华夏文脉数字展馆背景图-二十四节气.png'
+import coverMortise from '../picture/华夏文脉数字展馆背景图-榫卯建构.png'
+import coverPeople from '../picture/华夏文脉数字展馆背景图-民为邦本.png'
+import coverDragonBoat from '../picture/华夏文脉数字展馆背景图-端午年俗.png'
+import coverInnovation from '../picture/华夏文脉数字展馆背景图-革故鼎新.png'
 
 const props = defineProps({
   article: {
@@ -35,8 +42,25 @@ const viewCount = computed(() => props.article?.viewCount || 0)
 
 const coverFailed = ref(false)
 
+const localCoverMap = [
+  { matcher: /书法/, cover: coverCalligraphy },
+  { matcher: /端午|年俗/, cover: coverDragonBoat },
+  { matcher: /革故鼎新|创新/, cover: coverInnovation },
+  { matcher: /二十四节气|节气/, cover: coverSolarTerms },
+  { matcher: /榫卯|建构/, cover: coverMortise },
+  { matcher: /民为邦本|邦本|民本/, cover: coverPeople }
+]
+
+const localCoverUrl = computed(() => {
+  const text = `${props.article?.title || ''} ${props.article?.summary || ''} ${props.article?.categoryName || ''}`
+  const hit = localCoverMap.find((item) => item.matcher.test(text))
+  return hit ? hit.cover : coverDefault
+})
+
+const displayCoverUrl = computed(() => props.article?.coverUrl || localCoverUrl.value)
+
 watch(
-  () => props.article?.coverUrl,
+  () => [props.article?.coverUrl, props.article?.title],
   () => {
     coverFailed.value = false
   },
